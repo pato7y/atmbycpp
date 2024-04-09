fb4b23d2aea59f755f7905b2b540e6ede2b5ccfb
pato7y <serhatyildiz721@gmail.com>
2024-04-10 00:52:48 +0300

    night project is completed with c++

--- atm/main.cpp
+++ atm/main.cpp
@@ -1,7 +1,64 @@
 #include <iostream>
+using namespace std;
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void showMenu()
+{
+    cout << "*******MENU*******" << endl;
+    cout << "1. Check balance" << endl;
+    cout << "2. Deposit" << endl;
+    cout << "3. Withdraw" << endl;
+    cout << "4. Exit" << endl;
+    cout << "******************" << endl;
+}
 
 int main(int argc, char** argv) {
-	return 0;
+
+    double balance = 0.0; 
+    double withdrawAmount;
+    double depositAmount;
+    int option;
+
+    do {
+        showMenu();
+        cout << "Enter the option to continue: ";
+        cin >> option;
+        
+        switch (option) {
+            case 1:
+                cout << "Balance is $" << balance << endl;
+                break;
+
+            case 2:
+                cout << "Enter deposit amount: ";
+                cin >> depositAmount;
+                balance += depositAmount;
+                cout << "Deposit of $" << depositAmount << " successful." << endl;
+                break;
+
+            case 3:
+                cout << "Enter withdraw amount: ";
+                cin >> withdrawAmount;
+                if (withdrawAmount <= balance) {
+                    balance -= withdrawAmount;
+                    cout << "Withdraw of $" << withdrawAmount << " successful." << endl;
+                    cout << "Remaining balance is $" << balance << endl;
+                } else {
+                    cout << "Not enough money in the account." << endl;
+                }
+                break;
+
+            case 4:
+                cout << "Exiting..." << endl;
+                break;
+                system("cls");
+
+            default:
+                cout << "Invalid option. Please choose again." << endl;
+                //system("cls");
+
+        }
+    } while (option != 4);
+
+    return 0;
 }
+
